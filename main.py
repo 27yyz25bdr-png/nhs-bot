d704c6197b8ffc68090b3064488cc4ce5a5be2ad
27yyz25bdr-png <27yyz25bdr@privaterelay.appleid.com>
2026-06-09 00:53:07 +0500

    fix await

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+import asyncio
 
 TOKEN = "8884302613:AAHdNPfAk2P0l_hqvxRQ1-8F3K5jS9fiRzc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Bot is working.")
 
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-import asyncio
 async def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
     await app.run_polling()
 
-await main()
+if __name__ == "__main__":
+    asyncio.run(main())
